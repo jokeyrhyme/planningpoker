89c17fed4bb49b290010a5e518ffb357d277a673
Ron Waldon <jokeyrhyme@gmail.com>
2014-04-05 11:35:17 +1100

    remove experimental non-functioning code

--- js/RTCIceCandidate.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser:true, indent:2*/
-/*global define, require*/ // Require.JS
-
-define(function () {
-  'use strict';
-
-  return window.RTCIceCandidate || window.mozRTCIceCandidate;
-});
--- js/RTCPeerConnection.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser:true, indent:2*/
-/*global define, require*/ // Require.JS
-
-define(function () {
-  'use strict';
-
-  return window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-});
