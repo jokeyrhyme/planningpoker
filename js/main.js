b1fbe41a42c4975b37b2906577a816916fd9e2d6
Ron Waldon <jokeyrhyme@gmail.com>
2014-07-06 22:01:33 +1000

    progress made with Polymer planning-poker

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*jslint indent:2*/
+
+(function () {
+  'use strict';
+
+  var selected, selectedText, slider, t;
+
+  t = document.querySelector('#main');
+
+  t.fibonacci = [1, 2, 3, 5, 8, '\u221e'];
+
+  t.index = 0;
+
+}());
